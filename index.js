f5b72ec464bd8016390ab8d81b3931d416942755
Sabbasd21 <157584452+Sabbasd21@users.noreply.github.com>
2024-12-14 12:57:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["baddies", "/ethereals"];
+const statusMessages = ["helping /ethereals", "🎀/ethereals"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
